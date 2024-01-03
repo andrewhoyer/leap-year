839b84a472bfa44b96728c537f957aa14b28bcf9
admin <andrewhoyer@gmail.com>
2024-01-03 11:07:28 -0800

    Added 1900 and 2000 to output

--- Python/leap-year.py
+++ Python/leap-year.py
@@ -3,6 +3,8 @@ import datetime
 
 today = datetime.date.today()
 
-print(f"Is this year a leap year? {calendar.isleap(today.year)}")
+print(f"Is this year ({today.year}) a leap year? {calendar.isleap(today.year)}")
+print(f"Is 1900 a leap year? {calendar.isleap(1900)}")
+print(f"Is 2000 a leap year? {calendar.isleap(2000)}")
 print(f"Is 2023 a leap year? {calendar.isleap(2023)}")
 print(f"Is 2024 a leap year? {calendar.isleap(2024)}")
