b4fd689fa3e02406d416c38aad66d820e9657ae7
Heath Henley <heath.j.henley@gmail.com>
2024-01-06 13:05:57 -0500

    Make output match everyone elses

--- C++/leap-year.cpp
+++ C++/leap-year.cpp
@@ -13,7 +13,7 @@ bool is_leap_year(int year){
 }
 
 
-int main() {
+bool test() {
   // Testing the function - just grabbed a bunch randomly:
   std::vector<int> leap_years = {
     1804, 1820, 1844, 1852, 1940,
@@ -22,15 +22,27 @@ int main() {
   for (std::size_t i = 0; i < leap_years.size(); i++){
     if (!is_leap_year(leap_years[i])){
       std::cout << "Wrong answer for: " << leap_years[i] << std::endl;
-      return 1;
+      return false;
     }
     // we know leap_year + 1 isn't a leap year
     int def_not_leap_year = leap_years[i] + 1;
     if (is_leap_year(def_not_leap_year)) {
       std::cout << "Wrong answer for: " << def_not_leap_year << std::endl;
-      return 1;
+      return false;
     }
   }
-  std::cout << "All good!" << std::endl;
+  return true;
+}
+
+int main() {
+  if (!test()){
+    return 1;
+  }
+
+  std::vector<int> years_from_example = {1900, 2000, 2023, 2024};
+  for (size_t i = 0; i < years_from_example.size(); i++){
+    std::string ans = (is_leap_year(years_from_example[i])) ? "Yes!" : "No.";
+    std::cout << "Is " << years_from_example[i] << " a leap year? " << ans << std::endl;
+  }
   return 0;
 }
